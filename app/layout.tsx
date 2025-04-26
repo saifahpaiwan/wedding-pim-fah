a8ecf67c1c4fb9c079d5c01a7aa2d0eab5f5e920
saifahpaiwan <dev.saifah8953@gmail.com>
2025-04-26 21:39:47 +0700

    Update Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,30 @@ import type { Metadata } from "next";
 import "./globals.css";  
 
 export const metadata: Metadata = {
-  title: "Wedding Invitation | Sasithron & Saifah",
-  description: "invite you to join us in celebrating our wedding.",
+  title: 'Wedding Invitation | Sasithron & Saifah',
+  description: 'invite you to join us in celebrating our wedding.',
+  openGraph: {
+    type: 'website',
+    url: 'https://wedding-pim-fah.vercel.app/',
+    title: 'Wedding Invitation | Sasithron & Saifah',
+    description: 'invite you to join us in celebrating our wedding.',
+    images: [
+      {
+        url: '/images/Screenshot 2568-04-26 at 21.34.53.png',
+        width: 1200,
+        height: 630,
+        alt: 'Wedding Invitation',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Wedding Invitation | Sasithron & Saifah',
+    description: 'invite you to join us in celebrating our wedding.',
+    images: ['/images/Screenshot 2568-04-26 at 21.34.53.png'],
+  },
 };
+
  
 export default function RootLayout({
   children,
